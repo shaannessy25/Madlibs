2d71cb719084737f44001a704894d5be7f616c03
Shaan <Shaan@admins-MacBook-Pro.local>
2019-08-29 15:39:05 -0700

    added a function to get rid of all the global variables

--- madlibs.py
+++ madlibs.py
@@ -1,28 +1,31 @@
-# Got this madlib from hobbylark.com
+# Got this madlib from hobbylark.comde
 
-adjective = input("Enter an adjective: ")
-adjective2 = input("Enter another adjective: ")
-type_of_bird = input("Enter a type of bird: ")
-room = input("Enter a room: ")
-verb = input("Enter a past tense verb: ")
-verb2 = input("Enter a verb: ")
-relative = input("Enter a relatives name: ")
-noun = input("Enter a noun: ")
-liquid = input("Enter a type of liquid: ")
-verb3 = input("Enter a verb ending with ing: ")
-body_part = input("Enter a body part: ")
-noun2 = input("Enter a plural noun: ")
-verb4 = input("Enter a verb ending with ing: ")
-noun3 = input("Enter a noun: ")
+def user_input():
+    adjective = input("Enter an adjective: ")
+    adjective2 = input("Enter another adjective: ")
+    type_of_bird = input("Enter a type of bird: ")
+    room = input("Enter a room: ")
+    verb = input("Enter a past tense verb: ")
+    verb2 = input("Enter a verb: ")
+    relative = input("Enter a relatives name: ")
+    noun = input("Enter a noun: ")
+    liquid = input("Enter a type of liquid: ")
+    verb3 = input("Enter a verb ending with ing: ")
+    body_part = input("Enter a body part: ")
+    noun2 = input("Enter a plural noun: ")
+    verb4 = input("Enter a verb ending with ing: ")
+    noun3 = input("Enter a noun: ")
+    print("It was a " + adjective + ", cold November day.")
+    print("I woke up to the " + adjective2 + " smell of " + type_of_bird \
+    + " roasting in the " + room + " downstairs.")
+    print("I " + verb + " down the stairs to see if I could help \
+    " + verb2 + " the dinner.")
+    print("My mom said, 'See if " + relative + " needs a fresh " \
+    + noun + ".'")
+    print("So I carried a tray of glasses full of " + liquid + \
+    " into the " + verb3 + " room.")
+    print("When I got there, I could'nt believe my " + body_part + "!")
+    print("There were " + noun2 + " " + verb4 + " on the " + noun3 + "!")
 
-print("It was a " + adjective + ", cold November day.")
-print("I woke up to the " + adjective2 + " smell of " + type_of_bird \
-+ " roasting in the " + room + " downstairs.")
-print("I " + verb + " down the stairs to see if I could help \
-" + verb2 + " the dinner.")
-print("My mom said, 'See if " + relative + " needs a fresh " \
-+ noun + ".'")
-print("So I carried a tray of glasses full of " + liquid + \
-" into the " + verb3 + " room.")
-print("When I got there, I could'nt believe my " + body_part + "!")
-print("There were " + noun2 + " " + verb4 + " on the " + noun3 + "!")
+
+user_input()
